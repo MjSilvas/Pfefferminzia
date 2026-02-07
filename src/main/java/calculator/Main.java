66f40553c7c0ff0393b87d82c9b280dea40af368
Dominic Lenz <dominic.m.lenz@gmail.com>
2026-02-07 18:17:22 +0100

    made test not time sensitive

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -8,14 +8,7 @@ public class Main {
     static Calculator calc=new Calculator();
     public static void main(String[] args) {
 
-        BigDecimal big = new BigDecimal(3500);
-        BigDecimal big2 = new BigDecimal(2.5);
-        System.out.println(big.multiply(big2));
-        registerNewAnimal("Hund", "Premium", LocalDate.of(2019, 1, 1) );
-        registerNewAnimal("katze", "optimal", LocalDate.of(2023, 1, 1) );
-        registerNewAnimal("Hund", "Premium", LocalDate.of(2006, 1, 1) );
-        registerNewAnimal("kleintier", "kompakt", LocalDate.of(2025, 1, 1) );
-        registerNewAnimal("kleintier", "kompakt", LocalDate.of(2028, 1, 1) );
+        registerNewAnimal("katze", "Premium", LocalDate.of(2019, 1, 1) );
 
     }
 
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -38,10 +38,10 @@ Calculator calc = new Calculator();
     @Test
     public void calculatePremium_with_valid_Input(){
 
-        BigDecimal result1 = calc.calculatePremium("hund", "premium", LocalDate.of(2019, 1, 1));
-        BigDecimal result2 = calc.calculatePremium("katze", "optimal", LocalDate.of(2023, 1, 1));
-        BigDecimal result3 = calc.calculatePremium("kleintier", "kompakt", LocalDate.of(2025, 1, 1));
-        BigDecimal result4 = calc.calculatePremium("Hund", "premium", LocalDate.of(2000, 1, 1));
+        BigDecimal result1 = calc.calculatePremium("hund", "premium", LocalDate.now().minusYears(7));
+        BigDecimal result2 = calc.calculatePremium("katze", "optimal", LocalDate.now().minusYears(3));
+        BigDecimal result3 = calc.calculatePremium("kleintier", "kompakt", LocalDate.now().minusYears(1));
+        BigDecimal result4 = calc.calculatePremium("Hund", "premium", LocalDate.now().minusYears(26));
 
         assertEquals(new BigDecimal(980).setScale(2, RoundingMode.HALF_UP), result1);
         assertEquals(new BigDecimal(600).setScale(2, RoundingMode.HALF_UP), result2);
@@ -53,9 +53,9 @@ Calculator calc = new Calculator();
     @Test
     public void calculatePremium_with_invalid_Input(){
 
-        BigDecimal result1 = calc.calculatePremium("hund", "premium", LocalDate.of(2050, 1, 1));
-        BigDecimal result2 = calc.calculatePremium("hund", "super", LocalDate.of(2019, 1, 1));
-        BigDecimal result3 = calc.calculatePremium("pferd", "premium", LocalDate.of(2019, 1, 1));
+        BigDecimal result1 = calc.calculatePremium("hund", "premium", LocalDate.now().plusYears(2));
+        BigDecimal result2 = calc.calculatePremium("hund", "super", LocalDate.now().minusYears(7));
+        BigDecimal result3 = calc.calculatePremium("pferd", "premium", LocalDate.now().minusYears(7));
 
 
         assertEquals(new BigDecimal(700).setScale(2, RoundingMode.HALF_UP), result1);
